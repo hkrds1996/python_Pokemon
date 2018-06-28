43f6894cf633e080fa3759dea5e2d454254a2df8
HKR1996 <617980477@qq.com>
2018-06-28 21:57:49 +0800

    Update poekemondata.py

--- poekemondata.py
+++ poekemondata.py
@@ -6,45 +6,16 @@ Created on Sun Jun 17 16:23:55 2018
 """
 import urllib2,cookielib
 import os
+import re
 from bs4 import BeautifulSoup as bf
 import codecs
 import csv
-import requests
 import get_attri
 import time
 import random
 import math
-cj=cookielib.CookieJar();
-opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj));
-urllib2.install_opener(opener);      
-#创建Pokemon对象                
-class Pokemon(object):
-    def __init__(self,array,name,href):
-        self.array=array;
-        self.name=name;
-        self.href=href;
-    def ADD(self,HP,Attack,SPA,Defense,SPD,Speed,TTL,wei):
-        self.HP=HP;
-        self.Attack=Attack;
-        self.SPA=SPA;
-        self.Defense=Defense;
-        self.SPD=SPD;
-        self.Speed=Speed;
-        self.TTL=TTL;
-        self.wei=wei;
-pokemon=[];
-if(os.path.exists('pokemon.csv')):
-      csv_file=codecs.open('pokemon.csv','r',"utf-8-sig");
-      for stu in csv_file:
-          tm=stu.split(',');
-          poke=Pokemon(int(tm[0]),str(tm[1].encode('utf-8')),str(tm[2].encode('utf-8')));
-          poke.ADD(int(tm[3]),int(tm[4]),int(tm[5]),int(tm[6]),int(tm[7]),int(tm[8]),int(tm[9]),float(tm[10]));
-          pokemon.append(poke);                                  
-else:
-    url="http://www.pokemon.name/wiki/%E4%B8%BB%E9%A2%98:%E5%AE%9D%E5%8F%AF%E6%A2%A6";
-    #创建Request对象
-    request=urllib2.Request(url);
-    user_agent_list = [  
+from pokemon_class import Pokemon,Chara
+user_agent_list = [  
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",  
         "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",  
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",  
@@ -116,33 +87,84 @@ else:
         'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; Media Center PC 5.0; .NET CLR 2.0.50727)',  
         'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; WOW64; SLCC1; .NET CLR 3.0.04506)',  
        ];
-    request.add_header("User-Agent","Mozilla/5.0");
-    response=urllib2.urlopen(request);
-    soup=bf(response.read(),'html.parser',from_encoding='utf-8');
-    ccs=soup.find_all('td'); 
-    temp_list=0;                     
-    cow_=0;
-    for cc in ccs:
-        links=cc.find_all('li');
-        for link in links:        
-            if(len(link.get_text().split('. '))==2):
-                H=link.find('a');
-                Root="http://www.pokemon.name";
-                poke=Pokemon(int(link.get_text().split('. ')[0].split()[0].encode('utf-8')),link.get_text().split('. ')[1].encode('utf-8') ,(Root.decode('utf-8')+H['href']).encode('utf-8') );                
-                attrilist=get_attri.get_attri(poke.href,user_agent_list[int(math.floor(len(user_agent_list)*random.random()))]);
-                cow_=cow_+1;
-                time.sleep(1);
-                if(temp_list==len(user_agent_list)):
-                    temp_list=0;
-                poke.ADD(attrilist[0][0],attrilist[1][0],attrilist[2][0],attrilist[3][0],attrilist[4][0],attrilist[5][0],attrilist[6][0],attrilist[7][0]);
-                print poke.name,poke.HP,poke.array;
-                pokemon.append(poke);
-    csvFile = open("pokemon.csv", 'w+')
-    try:
-        csvFile.write(codecs.BOM_UTF8);
-        writer = csv.writer(csvFile);
-        for i in pokemon:
-            writer.writerow((i.array,i.name,i.href,i.HP,i.Attack,i.SPA,i.Defense,i.SPD,i.Speed,i.TTL,i.wei));
-    finally:
-        csvFile.close();
+def pokemondata():
+    cj=cookielib.CookieJar();
+    #seesion=requests.Session();
+    #headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit 537/36 (KHTML, like Gecko) Chrome",
+              #         "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"}
+    opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj));
+    urllib2.install_opener(opener);      
+    #创建Pokemon对象                
+    pokemon=[];
+    pokemon_r=[];
+    if(os.path.exists('pokemon.csv')):
+        csv_file=codecs.open('pokemon.csv','r',"utf-8-sig");
+        for stu in csv_file:
+            tm=stu.split(',');
+            poke=Pokemon(int(tm[0]),str(tm[1].encode('utf-8')),str(tm[2].encode('utf-8')));
+            attri_t=re.split('[/]',tm[12][1:])
+            texing_t=re.split('[/]',tm[13][1:]);
+            poke.ADD(str(tm[3].encode('utf-8')),int(tm[4]),int(tm[5]),int(tm[6]),int(tm[7]),int(tm[8]),int(tm[9]),int(tm[10]),float(tm[11]),attri_t,texing_t,str(tm[14].encode('utf-8')));
+            pokemon.append(poke);  
+        pokemon_r=pokemon;
+    else:
+        url="http://www.pokemon.name/wiki/%E4%B8%BB%E9%A2%98:%E5%AE%9D%E5%8F%AF%E6%A2%A6";
+        #创建Request对象
+        #q=session.get(url,headers=headers);
+        request=urllib2.Request(url);
+        
+        request.add_header("User-Agent","Mozilla/5.0");
+        response=urllib2.urlopen(request);
+        soup=bf(response.read(),'html.parser',from_encoding='utf-8');
+        #	soup=bf(req.text);
+        ccs=soup.find_all('td'); 
+        for cc in ccs:
+            links=cc.find_all('li');
+            for link in links:        
+                if(len(link.get_text().split('. '))==2):
+                    H=link.find('a');
+                    Root="http://www.pokemon.name";
+                    poke=Pokemon(int(link.get_text().split('. ')[0].split()[0].encode('utf-8')),link.get_text().split('. ')[1].encode('utf-8') ,(Root.decode('utf-8')+H['href']).encode('utf-8') );                
+                    m=get_attri.get_attri(poke.href,user_agent_list[int(math.floor(len(user_agent_list)*random.random()))]);
+                    print poke.name,poke.array,time.localtime(time.time());
+                    time.sleep(0.5*random.random());
+                    poke.ADD(m[-1],m[0],m[1],m[2],m[3],m[4],m[5],m[6],m[7],m[8],m[9],m[10]);
+                    pokemon.append(poke);
+        csvFile = open("pokemon.csv", 'w+')
+        try:
+            csvFile.write(codecs.BOM_UTF8);
+            writer = csv.writer(csvFile);
+            for i in pokemon:
+                for j in range(len(i.leixing)):
+                    attri_t=""
+                    for temp in i.attri[j]:
+                        attri_t=attri_t+"/"+str(temp);
+                    texing_t=""
+                    for temp in i.texing[j]:
+                        texing_t=texing_t+"/"+str(temp);
+                    writer.writerow((i.array,i.name,i.href,i.leixing[j],int(i.HP[j]),int(i.Attack[j]),int(i.SPA[j]),int(i.Defense[j]),int(i.SPD[j]),int(i.Speed[j]),int(i.TTL[j]),float(i.Weight[j]),attri_t,texing_t,i.hide[j]));
+                    pokemon_=Pokemon(i.array,i.name,i.href);
+                    pokemon_.ADD(i.leixing[j],int(i.HP[j]),int(i.Attack[j]),int(i.SPA[j]),int(i.Defense[j]),int(i.SPD[j]),int(i.Speed[j]),int(i.TTL[j]),float(i.Weight[j]),i.attri[j],i.texing[j],i.hide[j])
+                    pokemon_r.append(pokemon_)
+                
+        finally:
+            csvFile.close();
+    return pokemon_r;
 ###Pokemon属性
+def charadata():
+    cj=cookielib.CookieJar();
+    opener=urllib2.build_opener(urllib2.HTTPCookieProcessor(cj));
+    urllib2.install_opener(opener);  
+    chara=[];
+    if(os.path.exists('character.csv')):
+        csv_file=codecs.open('pokemon.csv','r',"utf-8-sig");
+        for stu in csv_file:
+            tm=stu.split(',');
+            char=Chara(str(tm[0]),str(tm[1]));
+            char.ADD(str(tm[2]),str(tm[3]));
+            chara.append(char);
+    else:
+        url="www.baidu.com";
+    
+    
+    
